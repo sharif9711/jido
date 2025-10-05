caf04c1f8b432af27035635e48ee6bc0995fcafd
sharif9711 <sharif9711@naver.com>
2025-10-05 22:25:16 +0900

    Delete js directory

--- js/ui.js
+++
@@ -1,4 +0,0 @@
-window.projectData = [
-  { no: 1, name: "홍길동", phone: "010-1111-2222", address: "서울 강남구", status: "예정", memo: [] },
-  { no: 2, name: "이순신", phone: "010-3333-4444", address: "부산 해운대구", status: "완료", memo: [] },
-];
