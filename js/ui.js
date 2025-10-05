5ce3b19953b4c9141b10e60cd13955f0d4d6604d
sharif9711 <sharif9711@naver.com>
2025-10-06 00:22:22 +0900

    Delete js directory

--- js/ui.js
+++
@@ -1,4 +0,0 @@
-export function showSection(id) {
-  document.querySelectorAll("section, main").forEach((el) => (el.style.display = "none"));
-  document.getElementById(id).style.display = "block";
-}
