e162af979edde366b9600e65dad4a5307ea1c15c
sharif9711 <sharif9711@naver.com>
2025-10-08 18:17:20 +0900

    Add files via upload

--- js/project-list.js
+++ js/project-list.js
@@ -33,12 +33,6 @@ function renderProjects() {
                                 <line x1="15" y1="6" x2="15" y2="21"></line>
                             </svg>
                         </div>
-                        <div class="p-2 rounded-lg bg-amber-100">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-amber-600">
-                                <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
-                                <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
-                            </svg>
-                        </div>
                     </div>
                     <div class="space-y-4">
                         <h3 class="text-lg font-semibold text-slate-900 line-clamp-1">${project.projectName}</h3>
