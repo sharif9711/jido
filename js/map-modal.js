7ce8cda9b84f8b996fd5ef7a0131f8e037119ffe
sharif9711 <sharif9711@naver.com>
2025-10-09 04:44:19 +0900

    Add files via upload

--- js/map-modal.js
+++ js/map-modal.js
@@ -4,12 +4,16 @@ function showMapView() {
     document.getElementById('normalView').style.display = 'none';
     document.getElementById('mapView').style.display = 'block';
     
-    // 지도 초기화
+    // 지도 초기화 (충분한 지연 시간)
     setTimeout(() => {
+        console.log('Initializing Kakao Map...');
         if (!kakaoMap) {
             initKakaoMap();
+        } else {
+            // 기존 지도가 있으면 relayout 호출
+            kakaoMap.relayout();
         }
-    }, 100);
+    }, 300);
 }
 
 function hideMapView() {
